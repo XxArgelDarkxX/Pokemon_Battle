03e84de1b79a5e62cb20c8022dda44d145f4b8b0
XxArgelDarkxX <castaneda.angel@correounivalle.edu.co>
2025-03-22 19:05:37 -0500

    Uso de comentarios en el archivo

--- Pokemon_Battle/src/Pokemon.java
+++ Pokemon_Battle/src/Pokemon.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 public class Pokemon {
     private String name;
     private enum Type {
-        FIRE, WATER, GRASS, GROUND,
+        FUEGO, AGUA, PLANTA, TIERRA,
     }
     private Type type;
     private short hp;
     private ArrayList moves;
 
-    
+    // Constructor de la clase Pokemon.
     public Pokemon(String name, Pokemon.Type type, short hp, ArrayList moves) {
         this.name = name;
         this.type = type;
@@ -49,20 +49,24 @@ public class Pokemon {
         this.moves = moves;
     }
     
+    // Este método permite a un Pokemon atacar a otro Pokemon.
     public void attack(Pokemon target, int moveIndex) {
         Move move = (Move) moves.get(moveIndex);
         double effectiveness = moveEffectiveness(this.type, target.getType());
-        target.setHp((short) (target.getHp() - (move.getPower() * effectiveness)));
+        target.setHp((short) (target.getHp() - (move.getPower() * effectiveness))); // Se resta la vida del Pokemon objetivo.
+        System.out.println(this.name + " ha usado" + move.getName() + "!");
     }
 
+    // Este método calcula la efectividad de un movimiento de un tipo de Pokemon sobre otro tipo de Pokemon.
+    // El metodo es privado porque solo va a ser usado por la clase Pokemon.
     private double moveEffectiveness(Type attacker, Type defender) {
-    if (attacker == Type.FIRE && defender == Type.GRASS) {
+    if (attacker == Type.FUEGO && defender == Type.PLANTA) {
         return 1.3;
-    } else if (attacker == Type.WATER && (defender == Type.FIRE || defender == Type.GROUND)) {
+    } else if (attacker == Type.AGUA && (defender == Type.FUEGO || defender == Type.TIERRA)) {
         return 1.3;
-    } else if (attacker == Type.GRASS && (defender == Type.WATER || defender == Type.GROUND)) {
+    } else if (attacker == Type.PLANTA && (defender == Type.AGUA || defender == Type.TIERRA)) {
         return 1.3;
-    } else if (attacker == Type.GROUND && defender == Type.FIRE) {
+    } else if (attacker == Type.TIERRA && defender == Type.FUEGO) {
         return 1.3;
     } else {
         return 1.0;
