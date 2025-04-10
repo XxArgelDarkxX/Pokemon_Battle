a6f6970a86966a4304ed48de40995e50cf741d28
KarenY93 <karen.sanabria@correounivalle.edu.co>
2025-04-09 21:20:17 -0500

    Excepciones en MovementMenu

--- Pokemon_Battle/src/Menu/MovementMenu.java
+++ Pokemon_Battle/src/Menu/MovementMenu.java
@@ -2,53 +2,69 @@ package Menu;
 
 import Movement.Movement;
 import Movement.Movement.Type;
+import java.util.Random;
 import java.util.Scanner;
 
-import javax.swing.JOptionPane;
-
 public class MovementMenu {
+    Scanner scanner;
+    Movement movement = new Movement();
+    Random random = new Random();
+    
+    //metodo para usar scanner
+    public MovementMenu(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
     public static void movementMenu(Scanner scanner) throws Exception {
         int numMovements = 4;
-        Movement[] movements = new Movement[numMovements];
-        
-        System.out.println("\nINGRESA TUS MOVIMIENTOS");
+        Movement[] movements = new Movement[numMovements];        
+
+        //Pedir informacion por consola
         for(int i=0; i<numMovements; i++){
             movements[i] = new Movement();
-            System.out.print("\nINGRESA EL NOMBRE DE TU MOVIMIENTO " + (i + 1) + "\n");
+            System.out.print("\nINGRESA EL NOMBRE DE TU MOVIMIENTO " + (i + 1) + ": ");
             String nameMovement = scanner.nextLine();
             scanner.nextLine();
             movements[i].setName(nameMovement);
 
-            System.out.println("""
-
-                               INGRESA EL TIPO DE TU MOVIMIENTO
-                               """);
-            System.out.println("1. FISICO\n 2. ESPECIAL\n"); //Mostramos las opciones
+            do{
+            System.out.println("\nINGRESA EL TIPO DE TU MOVIMIENTO\n");
+            System.out.println("1. FISICO\n2. ESPECIAL\n"); //Mostramos las opciones
             int option = scanner.nextInt();
             scanner.nextLine();
-            switch (option) {
-                case 1 -> movements[i].setType(Type.FISICO);
-                case 2 -> movements[i].setType(Type.ESPECIAL);
-                default -> System.out.println("Opcion no valida");
-            }
-
-            System.out.println("""
-
-                               INGRESA EL PODER DE TU MOVIMIENTO
-                               """);
-            System.out.print("MOVIMIENTO " + (i + 1) + ":\t");
-            int value ;
-            boolean flag;
-            do { 
-                value = scanner.nextInt();
-                if (value < 0 || value > 350) {
-                    System.out.println("El valor debe estar entre 0 y 100");
-                    flag = true;
-                }else{
-                    movements[i].setPower((byte)value);
-                    flag = false;
+            //Resive las opciones y verifica que sea valida
+                if (option == 1) {
+                    movements[i].setType(Type.FISICO);
+                    break; // Sale del bucle si la opción es válida
+                } else if (option == 2) {
+                    movements[i].setType(Type.ESPECIAL);
+                    break; // Sale del bucle si la opción es válida
+                } else {
+                    System.out.println("Opcion no valida, vuelve a ingresar");
                 }
-            } while (flag);
+            }while(true);
+
+        System.out.println("\nINGRESA EL PODER DE TU MOVIMIENTO\n");
+        System.out.print("MOVIMIENTO " + (i + 1) + ":\t");
+
+        int value;
+        boolean flag = true;  // Inicializar flag en true
+
+        do {
+            try {
+                value = scanner.nextInt();
+                
+                if (value < 0 || value > 100) {
+                    System.out.println("El valor debe estar entre 0 y 100"); //imprime un mensaje si la informacion ingresada es erronea
+                } else {
+                    movements[i].setPower((byte) value);
+                    flag = false;  // Salir del bucle si el valor es válido
+                }
+            } catch (Exception e) {
+                System.out.println("Entrada inválida. Debes ingresar un número.");
+                scanner.next();  // Limpiar la entrada incorrecta para evitar bucles infinitos
+            }
+        } while (flag);
         }
     }
 
--- Pokemon_Battle/src/Menu/PokemonMenu.java
+++ Pokemon_Battle/src/Menu/PokemonMenu.java
@@ -1,54 +1,82 @@
 package Menu;
 
-import Movement.Movement;
-import Movement.Movement.Type;
+import Data.Data;
+import Entrenador.Trainer;
+import Pokemon.Pokemon;
 import java.util.Scanner;
 
-public class MovementMenu {
-    public static void movementMenu(Scanner scanner) throws Exception {
-        int numMovements = 4;
-        Movement[] movements = new Movement[numMovements];
+public class PokemonMenu {
+    public static void pokemonMenu(Scanner scanner, Trainer trainers) throws Exception {
+
+        //definir la cantidad
+        int numPokemons = 3;
         
-        System.out.println("\nINGRESA TUS MOVIMIENTOS");
-        for(int i=0; i<numMovements; i++){
-            movements[i] = new Movement();
-            System.out.print("\nINGRESA EL NOMBRE DE TU MOVIMIENTO " + (i + 1) + "\n");
-            String nameMovement = scanner.nextLine();
-            scanner.nextLine();
-            movements[i].setName(nameMovement);
+        boolean flag;
+        Pokemon[] pokemons = new Pokemon[numPokemons];
+        int option ;
+        for(int i=0; i<numPokemons; i++){
+            //Ingresar el nombre
+            pokemons[i] = new Pokemon();
+            System.out.println("\nINGRESA EL NOMBRE DEL POKEMON\n");
+            System.out.print("POKEMON " + (i + 1) + ":\t");
+            String namePokemon = scanner.next();
+            pokemons[i].setName(namePokemon);
+            //Ingresar el tipo
+            System.out.println("\nINGRESE EL ELEMENTO DEL POKEMON ");
+            System.out.println(" 1. FUEGO\n 2. AGUA\n 3. PLANTA\n 4. TIERRA\n"); //Mostramos las opciones
+            
+            //legimos el tipo de pokemon
+            do{
+                flag = false;
+                    option = scanner.nextInt();
+                    switch (option) {
+                        case 1 -> pokemons[i].setType(Pokemon.Type.FUEGO);
+                        case 2 -> pokemons[i].setType(Pokemon.Type.AGUA);
+                        case 3 -> pokemons[i].setType(Pokemon.Type.PLANTA);
+                        case 4 -> pokemons[i].setType(Pokemon.Type.TIERRA);
+                        default -> {System.out.println("Opcion no valida");
+                            flag = true;}
+                    }
+            }while(flag);
 
-            System.out.println("""
-
-                               INGRESA EL TIPO DE TU MOVIMIENTO
-                               """);
-            System.out.println("1. FISICO\n 2. ESPECIAL\n"); //Mostramos las opciones
-            int option = scanner.nextInt();
-            scanner.nextLine();
-            switch (option) {
-                case 1 -> movements[i].setType(Type.FISICO);
-                case 2 -> movements[i].setType(Type.ESPECIAL);
-                default -> System.out.println("Opcion no valida");
-            }
-
-            System.out.println("""
-
-                               INGRESA EL PODER DE TU MOVIMIENTO
-                               """);
-            System.out.print("MOVIMIENTO " + (i + 1) + ":\t");
-            int value ;
-            boolean flag;
-            do { 
-                value = scanner.nextInt();
-                if (value < 0 || value > 350) {
-                    System.out.println("El valor debe estar entre 0 y 100");
+            //ingresamos el hp
+            do {
+                
+                try{
+                    short hp;
+                    System.out.println("\nINGRESA LA VIDA DEL POKEMON\n");
+                    System.out.print("POKEMON " + (i + 1) + ":\t");
+                    hp = scanner.nextShort();
+                    if(hp>350){
+                        flag = true;
+                    }else{
+                        pokemons[i].setHp(hp);
+                        flag = false;
+                    }
+                }catch(Exception e){
+                    System.out.println("ingrese un valor menor o igual a 350 de vida pendejo");
                     flag = true;
-                }else{
-                    movements[i].setPower((byte)value);
-                    flag = false;
+                    scanner.next();
+
                 }
             } while (flag);
+            System.out.println("""
+
+                               Selecciona los movimientos de tu Pokemon
+                               1. MOVIMIENTO ALEATORIO
+                               2. CREAR MOVIMIENTOS
+                               """);
+            do{
+                flag = false;
+                option = scanner.nextInt();
+                switch (option) {
+                    case 1 -> pokemons[i].setRandomMoves(Data.getMoves(pokemons[i].getType()));
+                    case 2 -> MovementMenu.movementMenu(scanner);
+                    default -> {System.out.println("Opcion no valida");
+                        flag = true;}
+                }
+            }while(flag);
+            trainers.getPokemonTeam().add(pokemons[i]);
         }
     }
-
-    
 }
